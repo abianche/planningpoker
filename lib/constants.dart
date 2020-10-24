5164a4fa26dc9a2b1adcddd4c4bf81631ae6548a
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2020-10-24 16:32:53 +0200

    add constants

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+const String PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.abianche.planningpoker';
+const String APP_STORE_URL = 'https://apps.apple.com/us/app/planning-poker-scrum/id1466098334?mt=8';
+const String PROFILE_URL = 'https://abianche.github.io/';
