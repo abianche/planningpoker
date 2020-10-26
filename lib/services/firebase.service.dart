cfb50c3af9adc11155d04abf2ab413802537e32c
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2020-10-26 18:39:33 +0100

    fix sign in

--- lib/services/firebase.service.dart
+++ lib/services/firebase.service.dart
@@ -2,11 +2,19 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:planningpoker/logger.dart';
 
 class FirebaseService {
+  static final FirebaseService _singleton = FirebaseService._internal();
+  FirebaseService._internal();
+
+  factory FirebaseService() {
+    return _singleton;
+  }
+
   final log = getLogger('FirebaseService');
+
   final auth = FirebaseAuth.instance;
 
   /// Sign-in anonymously to Firebase.
-  Future<User> signInAnonymously(AuthCredential credential) async {
+  Future<User> signInAnonymously() async {
     final result = await auth.signInAnonymously().catchError((error) {
       log.e('signInAnonymously | $error');
       signOut();
