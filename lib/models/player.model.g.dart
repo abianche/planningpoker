1af201abec1f570cc322989fe36a113cd9d7867e
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2021-12-27 23:04:20 +0100

    ⬆️ upgrade dependencies

--- lib/models/player.model.g.dart
+++ lib/models/player.model.g.dart
@@ -6,12 +6,10 @@ part of 'player.model.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Player _$PlayerFromJson(Map<String, dynamic> json) {
-  return Player(
-    username: json['username'] as String,
-    currentCard: json['currentCard'] as String,
-  );
-}
+Player _$PlayerFromJson(Map<String, dynamic> json) => Player(
+      username: json['username'] as String,
+      currentCard: json['currentCard'] as String,
+    );
 
 Map<String, dynamic> _$PlayerToJson(Player instance) => <String, dynamic>{
       'username': instance.username,
