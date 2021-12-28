eb4c2d10339b08166775dbee77cd21c132fd5f26
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2021-12-29 00:50:22 +0100

    🐛 fix 404 home button

--- lib/views/404.screen.dart
+++ lib/views/404.screen.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:planningpoker/router.dart';
 
 class NotFoundScreen extends StatelessWidget {
   final String? name;
@@ -13,22 +15,58 @@ class NotFoundScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Platform.isIOS
+    return !kIsWeb && Platform.isIOS
         ? CupertinoPageScaffold(
             navigationBar: CupertinoNavigationBar(
               middle: Text(AppLocalizations.of(context)!.pageNotFound),
             ),
-            child: Center(
-              child: Text('404 : $name'),
-            ),
+            child: Body(name: name),
           )
         : Scaffold(
             appBar: AppBar(
               title: Text(AppLocalizations.of(context)!.pageNotFound),
             ),
-            body: Center(
-              child: Text('404 : $name'),
-            ),
+            body: Body(name: name),
           );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({
+    Key? key,
+    required this.name,
+  }) : super(key: key);
+
+  final String? name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton.icon(
+            onPressed: () {
+              Navigator.of(context).pushReplacementNamed(Routes.home);
+            },
+            icon: const Icon(
+              Icons.home,
+              size: 32.0,
+            ),
+            label: const Text(
+              "Home",
+              textScaleFactor: 2.0,
+            ),
+          ),
+          const SizedBox(height: 5),
+          Opacity(
+            opacity: 0.5,
+            child: Text(
+              '404 : $name',
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
